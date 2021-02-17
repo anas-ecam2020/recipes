197b10b463db1ef427f5dd1c51ef0b7b2f1139b4
anas-ecam2020 <20316@ecam.be>
2021-02-17 01:16:54 +0100

    Create views & controller

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Recipe;
 
 class RecipesController extends AbstractController
 {
@@ -13,8 +15,84 @@ class RecipesController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this ->getDoctrine()->getRepository(Recipe::class);
+
+        $recipes = $repo -> findAll();
+
         return $this->render('recipes/index.html.twig', [
             'controller_name' => 'RecipesController',
+            'recipes' => $recipes
         ]);
     }
+    /**
+    * @Route("/recipes/{id}", name="recipe_show") 
+    */
+
+    public function show($id) {
+
+        $repo = $this ->getDoctrine() ->getRepository(Recipe::class);
+
+        $recipe = $repo ->find($id);
+
+
+        return $this ->render('recipes/show.html.twig', [
+                'recipe' => $recipe
+        ]);
+
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+
+     public function home() {
+
+      return $this ->render('recipes/home.html.twig');
+
+     }
+
+
+    /**
+     * @Route("/favorites", name="favorites")
+     */
+
+     public function favorites() {
+
+        $repo = $this ->getDoctrine() ->getRepository(Recipe::class);
+
+        $recipes = $repo ->findAll();
+
+
+        return $this ->render('recipes/favorites.html.twig', [
+                'recipes' => $recipes
+        ]);
+     }
+
+
+     /**
+      * @Route("/categories", name="categories")
+      */
+
+      public function categories() {
+
+        $repo = $this ->getDoctrine() ->getRepository(Category::class);
+
+        $categories = $repo ->findAll();
+
+        return $this ->render('recipes/categories.html.twig', [
+            'categories' => $categories
+    ]);
+      }
+
+
+      /**
+       * @Route("/add", name="add")
+       */
+
+
+      public function add() {
+
+        return $this ->render('recipes/add.html.twig');
+      }
+       
 }
